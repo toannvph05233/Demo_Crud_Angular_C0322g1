4726895b821cfa65f5183001bf734532ba522bc5
toannvph05233 <johntoan98@gmail.com>
2022-08-17 11:29:23 +0700

    up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from "../models/Product";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crudAngular';
+  products: Product[] = [];
+  product: Product = new Product(0,"","",0);
+
+  constructor() {
+    this.products.push(new Product(1,"oto","https://st.quantrimang.com/photos/image/2019/10/31/hinh-nen-o-to-dep-5.jpg",500))
+    this.products.push(new Product(2,"oto","https://storage.googleapis.com/f1-cms/2020/08/b93bd613-20200826_092520.jpg",500))
+    this.products.push(new Product(3,"oto","https://menback.com/wp-content/uploads/2022/01/xe-o-to-dat-nhat-the-gioi-menback.jpg",500))
+  }
+
+  create(): void{
+    this.products.push(this.product);
+    this.product = new Product(0,"","",0);
+  }
+
+  delete(index: number): void{
+    this.products.splice(index,1);
+  }
+
+
 }
