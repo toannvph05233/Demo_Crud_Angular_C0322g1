3e038c703083020722f77a10fabcdac5f098cd57
toannvph05233 <johntoan98@gmail.com>
2022-08-19 09:46:31 +0700

    upp template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from "../models/Product";
 
 @Component({
@@ -6,24 +6,29 @@ import {Product} from "../models/Product";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crudAngular';
   products: Product[] = [];
-  product: Product = new Product(0,"","",0);
-
+  product: Product = new Product(0, "", "", 0);
+  color = 'red';
   constructor() {
-    this.products.push(new Product(1,"oto","https://st.quantrimang.com/photos/image/2019/10/31/hinh-nen-o-to-dep-5.jpg",500))
-    this.products.push(new Product(2,"oto","https://storage.googleapis.com/f1-cms/2020/08/b93bd613-20200826_092520.jpg",500))
-    this.products.push(new Product(3,"oto","https://menback.com/wp-content/uploads/2022/01/xe-o-to-dat-nhat-the-gioi-menback.jpg",500))
   }
 
-  create(): void{
+
+  create(): void {
     this.products.push(this.product);
-    this.product = new Product(0,"","",0);
+    this.product = new Product(0, "", "", 0);
   }
 
-  delete(index: number): void{
-    this.products.splice(index,1);
+  delete(index: number): void {
+    this.products.splice(index, 1);
+  }
+
+  ngOnInit(): void {
+    this.products.push(new Product(1, "oto", "https://st.quantrimang.com/photos/image/2019/10/31/hinh-nen-o-to-dep-5.jpg", 400))
+    this.products.push(new Product(2, "oto", "https://storage.googleapis.com/f1-cms/2020/08/b93bd613-20200826_092520.jpg", 600))
+    this.products.push(new Product(3, "oto", "https://menback.com/wp-content/uploads/2022/01/xe-o-to-dat-nhat-the-gioi-menback.jpg", 700))
+
   }
 
 
